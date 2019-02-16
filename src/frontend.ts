cc32a7d2f184916070d2170959f8178449a4b958
ejb1123 <ejb1123@protonmail.com>
2019-02-16 15:03:08 -0500

    added jquery type stuff

--- src/frontend.ts
+++ src/frontend.ts
@@ -1,5 +1,6 @@
 import { UserManager } from './UserManager'
 import {SocketManager} from './SocketManager'
+import * as $ from 'jquery'
 let userman :UserManager = new UserManager();
 let letsockmanager :SocketManager = new SocketManager();
 letsockmanager.setUserConnectionEvents(userman);
